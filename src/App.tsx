be97d842c07f4fba03d20c087ebb6218d14057db
keshavram-apptware <keshavram.kuduwa@apptware.com>
2025-03-27 21:26:06 +0530

    Done with Customer Care Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { HelmetProvider } from "react-helmet-async";
+import Communication from "./pages/Communication";
 
 const App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const App: React.FC = () => {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/communication" element={<Communication />} />
         </Routes>
       </Router>
     </HelmetProvider>
